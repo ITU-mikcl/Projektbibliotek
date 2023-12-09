746469ef6312566e61e84a86319be2835f426433
Mikkel Clausen <151668241+ClausenMikkel@users.noreply.github.com>
2023-12-09 16:02:09 +0100

    1.0 StartTestnotWorkingYet

---
+++ src/packages/SpawnerTest.java
@@ -0,0 +1,13 @@
+package packages;
+import itumulator.world.World;
+import packages.Spawner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpawnerTest {
+    @Test
+    public static void test() {
+        Spawner spawner = new Spawner();
+    }
+}
+
